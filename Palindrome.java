50caf94d5ac6eb8092be778395e8509a5a52dfcf
Marina Moger <marina.moger@gmail.com>
2021-08-18 17:24:29 +0300

    Palindrome improved

--- Palindrome.java
+++ Palindrome.java
@@ -1,47 +1,36 @@
-package com.company;
+
 
 import java.util.LinkedList;
 import java.util.Stack;
 
-public class Palindrom {
+public class Palindrome {
 
     public static void main(String[] args) {
-        // write your code here
         String ourString = "Madam, in ,Eden, I’m Adam";
 
-
         Stack<Character> myStack = new Stack<>();
-      // Stack<Character> myQueue = new Stack<>();
         LinkedList<Character> myQueue = new LinkedList<>();
 
         String ourStringNew = ourString.toLowerCase();
         ourStringNew = ourStringNew.replaceAll("\\W", "");
         System.out.println(ourStringNew);
 
-
-
-      //  for (int i=0 ,j= ourStringNew.length(); i< ourString.length(); i++,j--) {
-            for (int i=0 ; i< ourStringNew.length(); i++) {
-            //myQueue.push(ourStringNew.charAt(j-1));
+        for (int i = 0; i < ourStringNew.length(); i++) {
             myQueue.add(ourStringNew.charAt(i));
             myStack.push(ourStringNew.charAt(i));
-
         }
         System.out.println(myQueue);
         System.out.println(myStack);
 
-
-        for (int i=0 ; i< myStack.size(); i++){
-            if (myQueue.peek() == myStack.peek())    {
+        for (int i = 0; i < myStack.size(); i++) {
+            if (myQueue.peek() == myStack.peek()) {
                 myQueue.pop();
                 myStack.pop();
-            }
-            else
-            {
+            } else {
                 System.out.println("It's not a palindrome :(");
                 break;
             }
-            if (i == myStack.size()){
+            if (i == myStack.size()) {
                 System.out.println("It is a palindrome");
             }
         }
